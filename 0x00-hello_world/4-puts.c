ae74850a9def09f1ff09fd0f944e12de381eace0
Ahmed Salem <ahmedxsalemx223308@gmail.com>
2023-02-20 17:39:33 -0800

    making betty happy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main is the starting point of the program
+ *
+ * a value of 0 is returned successfully
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
