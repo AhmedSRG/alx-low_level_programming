ac863dbaba00cedc60eea87675913388f14cecc3
Ahmed Salem <ahmedxsalemx223308@gmail.com>
2023-02-20 17:55:32 -0800

    betty is hard

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,11 @@
-#include <stdio.h>
-/* main: Entry point*/
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-/* return: always 0 (success)*/
 	return (0);
 }
